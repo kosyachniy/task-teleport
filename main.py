43468c293e19c7b15e4c7130f7084534dad96725
Алексей Полоз <polozhev@mail.ru>
2021-04-01 11:34:58 +0300

    List of postamates with a rare address

--- main.py
+++ main.py
@@ -9,6 +9,25 @@ with open('keys.json', 'r') as file:
 	TOKEN = json.loads(file.read())['token']
 
 
+# 1. Получить список постаматов с эндпоинта
 headers = {'Authorization': 'Token {}'.format(TOKEN)}
-res = requests.get(LINK, headers=headers).text
-print(res)
+res = requests.get(LINK, headers=headers).json()
+
+
+# 2. Выявить среди полученных постаматы с address_struct.fias_id редко встречающимся в базе (<10 раз)
+k = {}
+
+for postamat in res['results']:
+	id_ = postamat['id']
+	fias_id = postamat['location']['address_struct']['fias_id']
+
+	if fias_id not in k:
+		k[fias_id] = {id_}
+	else:
+		k[fias_id].add(id_)
+
+for postamat in k:
+	if len(k[postamat]) < 10:
+		print(*k[postamat], sep=' ', end=' ')
+
+print()
